912e6b6ee2df4aba6332c8d14d94c455ce62667e
j- <j@skeoh.com>
2017-03-21 14:01:43 +1100

    Declare some unit and value constants

---
+++ src/reducers/values.js
@@ -0,0 +1,20 @@
+/* Relative values */
+
+export const VALUE_LARGER = 'larger';   // 1.2
+export const VALUE_SMALLER = 'smaller'; // 1 / 1.2
+
+/* Absolute values */
+
+export const VALUE_XX_SMALL = 'xx-small'; // 9px
+export const VALUE_X_SMALL = 'x-small';   // 10px
+export const VALUE_SMALL = 'small';       // 13px
+export const VALUE_MEDIUM = 'medium';     // 16px
+export const VALUE_LARGE = 'large';       // 18px
+export const VALUE_X_LARGE = 'x-large';   // 24px
+export const VALUE_XX_LARGE = 'xx-large'; // 32px
+
+/* Other values */
+
+export const VALUE_INHERIT = 'inherit'; // 100%
+export const VALUE_INITIAL = 'initial'; // 16px
+export const VALUE_UNSET = 'unset';     // 100%
