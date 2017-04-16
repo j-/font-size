0220940731c7941c91bf93e2aeeb491b55dd05c0
j- <j@skeoh.com>
2017-04-17 00:43:49 +1000

    Use arrow keys to move focus up/down

--- src/components/ItemsStack.jsx
+++ src/components/ItemsStack.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import ItemRootContainer from './ItemRootContainer';
 import ItemContainer from './ItemContainer';
@@ -17,17 +17,63 @@ const getStackElements = (count) => {
 	return elements;
 };
 
-const ItemsStack = ({ count }) => (
-	<ol className="font-size-items">
-		<li value={ 0 } className="font-size-item font-size-item--root">
-			<ItemRootContainer />
-		</li>
-		{ getStackElements(count) }
-	</ol>
-);
+export default class ItemsStack extends Component {
+	constructor (props) {
+		super(props);
+		this.handleKeyDown = this.handleKeyDown.bind(this);
+	}
+
+	handleKeyDown (e) {
+		if (e.target.tagName !== 'INPUT') {
+			return;
+		}
+		switch (e.key) {
+			case 'ArrowUp': return this.handleArrowUp(e);
+			case 'ArrowDown': return this.handleArrowDown(e);
+		}
+	}
+
+	handleArrowUp (e) {
+		e.preventDefault();
+		this.focusPreviousInput(e);
+	}
+
+	handleArrowDown (e) {
+		e.preventDefault();
+		this.focusNextInput(e);
+	}
+
+	focusPreviousInput (e) {
+		const inputs = this.refs.items.querySelectorAll('input');
+		const index = [...inputs].findIndex((input) => input === e.target);
+		const prev = inputs[index - 1];
+		if (prev) {
+			prev.focus();
+		}
+	}
+
+	focusNextInput (e) {
+		const inputs = this.refs.items.querySelectorAll('input');
+		const index = [...inputs].findIndex((input) => input === e.target);
+		const next = inputs[index + 1];
+		if (next) {
+			next.focus();
+		}
+	}
+
+	render () {
+		const { count } = this.props;
+		return (
+			<ol ref="items" className="font-size-items" onKeyDown={ this.handleKeyDown }>
+				<li value={ 0 } className="font-size-item font-size-item--root">
+					<ItemRootContainer />
+				</li>
+				{ getStackElements(count) }
+			</ol>
+		);
+	}
+}
 
 ItemsStack.propTypes = {
 	count: PropTypes.number.isRequired,
 };
-
-export default ItemsStack;
