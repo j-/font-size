b36fea285ea5c6a5bd170addb4a0610e363aef60
j- <j@skeoh.com>
2017-04-16 16:50:30 +1000

    Add store to app

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from './reducers';
+
+const store = createStore(
+	reducer,
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
-	<App />,
+	<Provider store={ store }>
+		<App />
+	</Provider>,
 	document.getElementById('app')
 );
