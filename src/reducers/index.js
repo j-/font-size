fe291f05f5d80e9c0f6f7462070a4724c3bf1902
j- <j@skeoh.com>
2017-04-16 16:47:30 +1000

    Add stack reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import defaultSize, * as defaultSizeModule from './reducer-default-size';
+import stack, * as stackModule from './reducer-stack';
 
 export default combineReducers({
 	defaultSize,
+	stack,
 });
 
 export const getDefaultFontSize = (state) => (
---
+++ src/reducers/reducer-stack.js
@@ -0,0 +1,10 @@
+const DEFAULT_STATE = {
+	items: [],
+};
+
+export default (state = DEFAULT_STATE, action) => {
+	switch (action.type) {
+		default:
+			return state;
+	}
+};
